3ff2f898196bea91369ec04160ba8851e9946ca5
Ben DiFrancesco <ben@scopelift.co>
2026-05-13 17:41:27 -0400

    Avoid dupelication and unneccessary collapse of side menu headers

--- pages/_meta.js
+++ pages/_meta.js
@@ -5,27 +5,27 @@ export default {
     "type": "separator",
     "title": "Token sales"
   },
-  "token-sales": "Token sales",
+  "token-sales": { "title": "Token sales", "display": "children" },
   "-- sep-1": {
     "type": "separator",
     "title": "On-chain operations"
   },
-  "on-chain-operations": "On-chain operations",
+  "on-chain-operations": { "title": "On-chain operations", "display": "children" },
   "-- sep-2": {
     "type": "separator",
     "title": "How to Use Tally"
   },
-  "how-to-use-tally": "How to Use Tally",
+  "how-to-use-tally": { "title": "How to Use Tally", "display": "children" },
   "-- sep-3": {
     "type": "separator",
     "title": "Set up & Technical Documentation"
   },
-  "set-up-and-technical-documentation": "Set up & Technical Documentation",
+  "set-up-and-technical-documentation": { "title": "Set up & Technical Documentation", "display": "children" },
   "-- sep-4": {
     "type": "separator",
     "title": "Education"
   },
-  "user-guides": "Education",
+  "user-guides": { "title": "Education", "display": "children" },
   "-- sep-5": {
     "type": "separator",
     "title": "Resources"
